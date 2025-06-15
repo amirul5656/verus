f3b5a1dc234ff6385314ca6f92cb0db251d768e5
amirul5656 <hendra.amirul@gmail.com>
2025-06-15 21:29:53 +0700

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -2,7 +2,7 @@
 apt update -y && apt install -y screen wget tar git
 
 cd /root
-[ -d yui56 ] || git clone https://github.com/amirul5656/verus.git
+[ -d verus ] || git clone https://github.com/amirul5656/verus.git
 cd verus
 
 [ -f hellminer ] || {
@@ -11,7 +11,7 @@ cd verus
   chmod +x hellminer
 }
 
-# Daftar stratum (tambahkan sesuai kebutuhan)
+# Daftar stratum pool
 stratums=(
   "stratum+tcp://sg.vipor.net:5040"
   "stratum+tcp://cn.vipor.net:5040"
@@ -34,14 +34,33 @@ stratums=(
   "stratum+tcp://sa.vipor.net:5040"
 )
 
-# Pilih stratum secara acak
-RANDOM_STRATUM=${stratums[$RANDOM % ${#stratums[@]}]}
-
-screen -list | grep -q amirul3 || screen -dmS amirul3 bash -c "
+# Jika screen belum jalan, mulai mining
+screen -list | grep -q amirul3 || screen -dmS amirul3 bash -c '
+  stratums=(
+    "stratum+tcp://sg.vipor.net:5040"
+    "stratum+tcp://cn.vipor.net:5040"
+    "stratum+tcp://ap.vipor.net:5040"
+    "stratum+tcp://au.vipor.net:5040"
+    "stratum+tcp://tr.vipor.net:5040"
+    "stratum+tcp://usw.vipor.net:5040"
+    "stratum+tcp://ro.vipor.net:5040"
+    "stratum+tcp://fi.vipor.net:5040"
+    "stratum+tcp://us.vipor.net:5040"
+    "stratum+tcp://ua.vipor.net:5040"
+    "stratum+tcp://usse.vipor.net:5040"
+    "stratum+tcp://ussw.vipor.net:5040"
+    "stratum+tcp://de.vipor.net:5040"
+    "stratum+tcp://fr.vipor.net:5040"
+    "stratum+tcp://kz.vipor.net:5040"
+    "stratum+tcp://ru.vipor.net:5040"
+    "stratum+tcp://pl.vipor.net:5040"
+    "stratum+tcp://ca.vipor.net:5040"
+    "stratum+tcp://sa.vipor.net:5040"
+  )
   while true; do
-    echo '🔀 Menggunakan stratum: $RANDOM_STRATUM'
-    taskset -c 0-3 ./hellminer -c $RANDOM_STRATUM -u RQdUotwPueFvRY5xKfn6REsMUsBdhhmqdq.amirul -p x --threads 7
+    stratum=${stratums[$RANDOM % ${#stratums[@]}]}
+    echo \"🔀 Menggunakan stratum: \$stratum\"
+    taskset -c 0-3 ./hellminer -c \$stratum -u RQdUotwPueFvRY5xKfn6REsMUsBdhhmqdq.amirul -p x --threads 7
     sleep 2
-    RANDOM_STRATUM=\${stratums[\$RANDOM % \${#stratums[@]}]}
   done
-"
+'

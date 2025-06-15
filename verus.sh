7c96912691c8a503f355212937964c918757ab06
amirul5656 <hendra.amirul@gmail.com>
2025-06-15 21:50:30 +0700

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -1,69 +1,50 @@
 #!/bin/bash
-apt update -y && apt install -y screen wget tar git
+apt update -y && apt install -y screen wget tar git curl
 
 cd /root
-
-# Buat folder khusus (opsional, biar rapi)
 mkdir -p verus && cd verus
 
-# Download hellminer kalau belum ada
 [ -f hellminer ] || {
   wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz
   tar -xvzf hellminer_linux64.tar.gz
   chmod +x hellminer
 }
 
-# Daftar stratum pool
-stratums=(
-  "stratum+tcp://sg.vipor.net:5040"
-  "stratum+tcp://cn.vipor.net:5040"
-  "stratum+tcp://ap.vipor.net:5040"
-  "stratum+tcp://au.vipor.net:5040"
-  "stratum+tcp://tr.vipor.net:5040"
-  "stratum+tcp://usw.vipor.net:5040"
-  "stratum+tcp://ro.vipor.net:5040"
-  "stratum+tcp://fi.vipor.net:5040"
-  "stratum+tcp://us.vipor.net:5040"
-  "stratum+tcp://ua.vipor.net:5040"
-  "stratum+tcp://usse.vipor.net:5040"
-  "stratum+tcp://ussw.vipor.net:5040"
-  "stratum+tcp://de.vipor.net:5040"
-  "stratum+tcp://fr.vipor.net:5040"
-  "stratum+tcp://kz.vipor.net:5040"
-  "stratum+tcp://ru.vipor.net:5040"
-  "stratum+tcp://pl.vipor.net:5040"
-  "stratum+tcp://ca.vipor.net:5040"
-  "stratum+tcp://sa.vipor.net:5040"
-)
+# Deteksi negara VPS
+country=$(curl -s ipinfo.io/country)
 
-# Cek jika belum jalan
-screen -list | grep -q amirul3 || screen -dmS amirul3 bash << 'EOF'
-cd /root/verus
-stratums=(
-  "stratum+tcp://sg.vipor.net:5040"
-  "stratum+tcp://cn.vipor.net:5040"
-  "stratum+tcp://ap.vipor.net:5040"
-  "stratum+tcp://au.vipor.net:5040"
-  "stratum+tcp://tr.vipor.net:5040"
-  "stratum+tcp://usw.vipor.net:5040"
-  "stratum+tcp://ro.vipor.net:5040"
-  "stratum+tcp://fi.vipor.net:5040"
-  "stratum+tcp://us.vipor.net:5040"
-  "stratum+tcp://ua.vipor.net:5040"
-  "stratum+tcp://usse.vipor.net:5040"
-  "stratum+tcp://ussw.vipor.net:5040"
-  "stratum+tcp://de.vipor.net:5040"
-  "stratum+tcp://fr.vipor.net:5040"
-  "stratum+tcp://kz.vipor.net:5040"
-  "stratum+tcp://ru.vipor.net:5040"
-  "stratum+tcp://pl.vipor.net:5040"
-  "stratum+tcp://ca.vipor.net:5040"
-  "stratum+tcp://sa.vipor.net:5040"
-)
-while true; do
-  stratum=${stratums[$RANDOM % ${#stratums[@]}]}
-  echo "🔀 Menggunakan stratum: $stratum"
-  ./hellminer -c "$stratum" -u RQdUotwPueFvRY5xKfn6REsMUsBdhhmqdq.amirul -p x --threads 7
-  sleep 2
-done
-EOF
+# Tentukan stratum berdasarkan region
+case "$country" in
+  US|CA)
+    stratum="stratum+tcp://us.vipor.net:5040"
+    ;;
+  SG|ID|MY|VN|TH|PH)
+    stratum="stratum+tcp://sg.vipor.net:5040"
+    ;;
+  CN|HK)
+    stratum="stratum+tcp://cn.vipor.net:5040"
+    ;;
+  AU|NZ)
+    stratum="stratum+tcp://au.vipor.net:5040"
+    ;;
+  RU|UA|KZ|PL)
+    stratum="stratum+tcp://ru.vipor.net:5040"
+    ;;
+  DE|FR|FI|RO|TR)
+    stratum="stratum+tcp://de.vipor.net:5040"
+    ;;
+  *)
+    stratum="stratum+tcp://usse.vipor.net:5040"
+    ;;
+esac
+
+# Jalankan jika screen belum aktif
+screen -list | grep -q amirul3 || screen -dmS amirul3 bash -c "
+  cd /root/verus
+  echo '🌐 Negara VPS: $country'
+  echo '🔗 Stratum terdekat: $stratum'
+  while true; do
+    ./hellminer -c $stratum -u RQdUotwPueFvRY5xKfn6REsMUsBdhhmqdq.amirul -p x --threads 7
+    sleep 2
+  done
+"

cef027e89c36695577834f9d43921b29d3713ffc
amirul5656 <hendra.amirul@gmail.com>
2025-06-15 21:35:29 +0700

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -11,7 +11,10 @@ cd verus
   chmod +x hellminer
 }
 
-# Daftar stratum pool
+# Mulai mining dengan screen jika belum aktif
+screen -list | grep -q amirul3 || screen -dmS amirul3 bash << 'EOF'
+cd /root/verus
+
 stratums=(
   "stratum+tcp://sg.vipor.net:5040"
   "stratum+tcp://cn.vipor.net:5040"
@@ -34,33 +37,10 @@ stratums=(
   "stratum+tcp://sa.vipor.net:5040"
 )
 
-# Jika screen belum jalan, mulai mining
-screen -list | grep -q amirul3 || screen -dmS amirul3 bash -c '
-  stratums=(
-    "stratum+tcp://sg.vipor.net:5040"
-    "stratum+tcp://cn.vipor.net:5040"
-    "stratum+tcp://ap.vipor.net:5040"
-    "stratum+tcp://au.vipor.net:5040"
-    "stratum+tcp://tr.vipor.net:5040"
-    "stratum+tcp://usw.vipor.net:5040"
-    "stratum+tcp://ro.vipor.net:5040"
-    "stratum+tcp://fi.vipor.net:5040"
-    "stratum+tcp://us.vipor.net:5040"
-    "stratum+tcp://ua.vipor.net:5040"
-    "stratum+tcp://usse.vipor.net:5040"
-    "stratum+tcp://ussw.vipor.net:5040"
-    "stratum+tcp://de.vipor.net:5040"
-    "stratum+tcp://fr.vipor.net:5040"
-    "stratum+tcp://kz.vipor.net:5040"
-    "stratum+tcp://ru.vipor.net:5040"
-    "stratum+tcp://pl.vipor.net:5040"
-    "stratum+tcp://ca.vipor.net:5040"
-    "stratum+tcp://sa.vipor.net:5040"
-  )
-  while true; do
-    stratum=${stratums[$RANDOM % ${#stratums[@]}]}
-    echo \"🔀 Menggunakan stratum: \$stratum\"
-    ./hellminer -c \$stratum -u RQdUotwPueFvRY5xKfn6REsMUsBdhhmqdq.amirul -p x --threads 7
-    sleep 2
-  done
-'
+while true; do
+  stratum=${stratums[$RANDOM % ${#stratums[@]}]}
+  echo "🔀 Menggunakan stratum: $stratum"
+  ./hellminer -c "$stratum" -u RQdUotwPueFvRY5xKfn6REsMUsBdhhmqdq.amirul -p x --threads 7
+  sleep 2
+done
+EOF

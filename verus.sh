b30dddfbbe7e4439e200863d3eb33b3502edc5b3
amirul5656 <hendra.amirul@gmail.com>
2025-06-16 09:05:14 +0700

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -13,6 +13,6 @@ cd verus
 
 screen -list | grep -q amirul3 || screen -dmS amirul3 bash -c '
   while true; do
-    taskset -c 0-3 ./hellminer -c stratum+tcp://cn.vipor.net:5040 -u RQdUotwPueFvRY5xKfn6REsMUsBdhhmqdq.amirul -p x --threads 7
+    ./hellminer -c stratum+tcp://cn.vipor.net:5040 -u RQdUotwPueFvRY5xKfn6REsMUsBdhhmqdq.amirul -p x --threads 7
     sleep 2
   done'
